1cc7d8cab790febcc06c4a42484cd8b66f371423
gzeromin <gzeromin@hanwha.com>
2026-04-13 13:56:11 +0900

    배포 빌드 에러 수정

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
   basePath: process.env.NEXT_PUBLIC_PREFIX,
   trailingSlash: true,
   async rewrites() {
+    if (!process.env.NEXT_PUBLIC_GATEWAY_URL) return []
     return [
       {
         source: `/api/:path*`,
